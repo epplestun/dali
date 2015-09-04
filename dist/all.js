b0c6f78247c2890987200c961dfc9fe9fcc2f0dc
epplestun <ivan.rodriguez@buntplanet.com>
2015-09-04 08:30:58 +0200

    Second commit

---
+++ dist/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Dali = function Dali() {
+	_classCallCheck(this, Dali);
+};
+//# sourceMappingURL=all.js.map
