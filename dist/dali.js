6ccadb366769856b80b0aef109e761df75a1bab4
epplestun <ivan.rodriguez@buntplanet.com>
2015-09-04 15:58:08 +0200

    System import

--- dist/dali.js
+++ dist/dali.js
@@ -1,35 +1,74 @@
-export function bootstrap(target) {
+'use strict';
+'use strict';
+
+function bootstrap(target) {
   var injector = new Injector();
   var instance = injector.get(target);
   instance.run();
 }
-export class Injector {  
-  resolve(target) {
-    var dependencies= [];
-    
-    if(!!target.dependencies) {
-      dependencies = target.dependencies.map(function(target) {
-        return this.resolve(target);
-      }.bind(this));
+"use strict";
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Injector = (function () {
+  function Injector() {
+    _classCallCheck(this, Injector);
+  }
+
+  _createClass(Injector, [{
+    key: "resolve",
+    value: function resolve(target) {
+      var dependencies = [];
+
+      if (!!target.dependencies) {
+        dependencies = target.dependencies.map((function (target) {
+          return this.resolve(target);
+        }).bind(this));
+      }
+
+      var proto = target.prototype;
+      var instance = Object(proto) === proto ? Object.create(proto) : {};
+      var result = Function.prototype.apply.call(target, instance, dependencies);
+      return Object(result) === result ? result : instance;
     }
-    
-    var proto = target.prototype;
-    var instance = (Object(proto) === proto) ? Object.create(proto) : {};
-    var result = Function.prototype.apply.call(target, instance, dependencies);
-    return Object(result) === result ? result : instance;  
+  }, {
+    key: "get",
+    value: function get(target) {
+      return this.resolve(target);
+    }
+  }]);
+
+  return Injector;
+})();
+
+function Inject() {
+  for (var _len = arguments.length, values = Array(_len), _key = 0; _key < _len; _key++) {
+    values[_key] = arguments[_key];
   }
-  
-  get(target) {
-    return this.resolve(target);
-  }
-}
-export function Inject(...values) {
-  return function(target) {
+
+  return function (target) {
     target.dependencies = values;
-  }
+  };
 }
-export class HTTP {
-	get() {
-		console.log('HTTP GET');
+'use strict';
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var HTTP = (function () {
+	function HTTP() {
+		_classCallCheck(this, HTTP);
 	}
-}
+
+	_createClass(HTTP, [{
+		key: 'get',
+		value: function get() {
+			console.log('HTTP GET');
+		}
+	}]);
+
+	return HTTP;
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,18 @@
 var gulp = require('gulp');
 var del = require('del');
-var Builder = require('systemjs-builder');
-var builder = new Builder();
 var concat = require('gulp-concat');
+var babel = require('gulp-babel');
 
 gulp.task('clean', function(cb) {
   return del(['dist'], cb);
 });
  
-gulp.task('default', ['clean'], function() {
-	return gulp.src('src/**/*.js')
-    .pipe(concat('dali.js'))
-    .pipe(gulp.dest('dist'));
+gulp.task('default', ['clean'], function () {
+  return gulp.src('src/**/*.js')
+      .pipe(babel({ 
+      	optional: ["es7.decorators"],
+      	modules: 'ignore'
+      }))
+      .pipe(concat('dali.js'))
+      .pipe(gulp.dest('dist'));
 });
-
-/*
-gulp.task('builder', ['clean'], function (cb) {
-	builder.buildSFX('dali', 'dist/dali.js', {
-		sourceMaps: false,
-		minify: true,
-		config: {
-			baseURL: 'src',
-			defaultJSExtensions: true,
-			transpiler: 'babel',
-			experimental: true,
-			babelOptions: {
-				optional: ["es7.decorators"]	
-			}	
-		}
-	}).then( function() {
-		cb();
-	}, function( err ) {
-		console.log( err );
-		cb();
-	});
-});
-
-gulp.task('default', ['builder']);
-*/
