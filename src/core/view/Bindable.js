174110ac91b39df38bf5db44d04b31d4c055c875
ivan <epplestun@MacBook-Air-de-Leire.local>
2015-09-20 14:25:40 +0200

    Bindable

--- src/core/view/Bindable.js
+++ src/core/view/Bindable.js
@@ -1,61 +1,13 @@
 import {EventBus, EventNameNormalizer} from 'core/event/EventBus';
 import {Injector} from 'core/di/Injector';
 
-/*
- Object.defineProperty(Array.prototype, "push", {
- configurable: false,
- enumerable: false, // hide from for...in
- writable: false,
- value: function () {
-
- for (var i = 0, n = this.length, l = arguments.length; i < l; i++, n++) {
- this[n] = arguments[i];
- //console.log(this, n, this[n] = arguments[i]); // assign/raise your event
- }
-
- console.log(this, n);
-
- //EventBus.publish(eventName);
-
- return n;
- }
- });
- */
-
-/*
- var observer = new MutationObserver(function(mutations) {
- mutations.forEach(function(mutation) {
- console.log(mutation);
- });
- });
-
- var config = { attributes: true, childList: true, characterData: true };
-
- // pass in the target node, as well as the observer options
- observer.observe(target, config);
- */
-
-
 export function Bindable(target, key) {
   if(target.bindableFields) {
     target.bindableFields.push(key);
   } else {
     target.bindableFields = [key];
   }
-  /*
-  let setter = descriptor.set;
-  let eventName = EventNameNormalizer.normalize(
-    target.constructor, EventBus.CHANGE_DETECTED
-  );
-
-  descriptor.set = function (value) {
-    setter.call(this, value);
-    EventBus.publish(eventName);
-  };
-  */
 }
-
-
 
 export class Binder {
 
