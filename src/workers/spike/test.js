088f88c4e08aac676f0ea284882e0fc0c1e4f0d2
ivan <epplestun@MacBook-Air-de-Leire.local>
2015-10-05 23:20:52 +0200

    Web Worker Spike

---
+++ src/workers/spike/test.js
@@ -0,0 +1,21 @@
+function HTTP() {
+  this.worker = new Worker("worker.js");
+
+  this.worker.addEventListener('message', function(e) {
+    this.callback(e.data);
+  }.bind(this), false);
+}
+
+HTTP.prototype.get = function(url, callback) {
+  this.callback = callback;
+  this.worker.postMessage({method: 'GET', url: url});
+};
+
+
+var http = new HTTP();
+
+document.getElementById('b').addEventListener('click', function() {
+  http.get('data.json', function(response) {
+    console.log(response);
+  });
+}, false);
---
+++ src/workers/spike/worker.js
@@ -0,0 +1,9 @@
+self.addEventListener('message', function(e) {
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", function() {
+    self.postMessage(this.responseText);
+  });
+  xhr.overrideMimeType("text/json; charset=UTF8");
+  xhr.open(e.data.method, e.data.url);
+  xhr.send();
+}, false);
