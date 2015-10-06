5f566b6d0accd87c3c77c938f6ad73abf19f294d
epplestun <ivan.rodriguez@buntplanet.com>
2015-10-06 15:00:38 +0200

    Tests

--- test/core/util/util_test.js
+++ test/core/util/util_test.js
@@ -34,7 +34,8 @@ describe('Utils', function() {
     it('should return valid guid', function () {
       let uuid = guid();
 
-      assert.equal(true, 
+      assert.equal(
+        true, 
         /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(uuid)
       );
     });
