7e4e7ae96f7072b83892ac14493a411666c261ea
ivan <epplestun@MacBook-Air-de-Leire.local>
2015-10-09 23:27:10 +0200

    Tests

---
+++ test/core/event/event_test.js
@@ -0,0 +1,29 @@
+describe('EventBus', () => {
+  describe('#subscribe()', () => {
+    it('...');
+  });
+
+  describe('#unsubscribe()', () => {
+    it('...');
+  });
+
+  describe('#publish()', () => {
+    it('...');
+  });
+});
+
+describe('EventNameNormalizer', () => {
+  describe('#normalize()', () => {
+    it('...');
+  });
+});
+
+describe('EventBinder', () => {
+  describe('#bindInstance()', () => {
+    it('...');
+  });
+
+  describe('#bind()', () => {
+    it('...');
+  });
+});
---
+++ test/core/filters/filters_test.js
@@ -0,0 +1,14 @@
+describe('Filter', () => {
+  it('...');
+});
+
+describe('Filters', () => {
+  describe('#get()', () => {
+    it('...');
+  });
+
+  describe('#getFilters()', () => {
+    it('...');
+  });
+});
+
