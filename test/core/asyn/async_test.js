3bf08b2f32f5128cf83c5d64168657c3ffb472fd
ivan <ivan.rodriguez@buntplanet.com>
2015-10-15 21:36:34 +0200

    Prepare async test

--- test/core/asyn/async_test.js
+++ test/core/asyn/async_test.js
@@ -1,7 +1,18 @@
+import {AsyncTask} from 'core/async/AsyncTask';
+
 var assert = require("assert");
 
 describe.skip('AsyncTask', () => {
   describe('#execute()', () => {
-    it('should execute task in background');
+    it('should execute task in background', () => {
+      let task = new AsyncTask((a, b) => {
+        return a + b;
+      });
+
+      task.execute(2, 2).then((result) => {
+        //console.log('Result', result);
+        assert.equal(result, 4);
+      });
+    });
   });
 });
