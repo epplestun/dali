f2ae0e51bc9f4f6954fbb9194dfbb7aacab2ee71
ivan <epplestun@MacBook-Air-de-Leire.local>
2015-11-03 22:14:53 +0100

    Refactor

--- src/core/async/AsyncTask.js
+++ src/core/async/AsyncTask.js
@@ -1,4 +1,4 @@
-import {decorate} from 'core/util/util';
+import {decorate, cloneFunc} from 'core/util/util';
 import {WorkerMock} from 'core/mocks/WorkerMock';
 
 function isNode() {
@@ -7,20 +7,6 @@ function isNode() {
 
 function hasWorkerSupport() {
   return (typeof window.Worker !== 'undefined' && typeof window.Blob !== 'undefined') && (typeof window.URL.createObjectURL == 'function')
-}
-
-function cloneFunc( func ) {
-  var reFn = /^function\s*([^\s(]*)\s*\(([^)]*)\)[^{]*\{([^]*)\}$/gi
-    , s = func.toString().replace(/^\s|\s$/g, '')
-    , m = reFn.exec(s);
-  if (!m || !m.length) return;
-  var conf = {
-    name : m[1] || '',
-    args : m[2].replace(/\s+/g,'').split(','),
-    body : m[3] || ''
-  }
-  var clone = Function.prototype.constructor.apply(this, [].concat(conf.args, conf.body));
-  return clone;
 }
 
 export function Async(target) {
--- src/core/util/util.js
+++ src/core/util/util.js
@@ -87,5 +87,18 @@ export function merge(obj1, obj2) {
   return Object.assign(obj1, obj2);
 }
 
+export function cloneFunc( func ) {
+  var reFn = /^function\s*([^\s(]*)\s*\(([^)]*)\)[^{]*\{([^]*)\}$/gi
+    , s = func.toString().replace(/^\s|\s$/g, '')
+    , m = reFn.exec(s);
+  if (!m || !m.length) return;
+  var conf = {
+    name : m[1] || '',
+    args : m[2].replace(/\s+/g,'').split(','),
+    body : m[3] || ''
+  }
+  var clone = Function.prototype.constructor.apply(this, [].concat(conf.args, conf.body));
+  return clone;
+}
 
 log('util.js');
